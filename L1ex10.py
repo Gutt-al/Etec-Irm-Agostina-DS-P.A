19bf5185cb454389a2885078fc01c1424fb35a3d
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:40:48 -0300

    Add files via upload

---
+++ L1ex10.py
@@ -0,0 +1,11 @@
+# Crie um algoritmo que leia um numero, divida o seu valor por 2 e atribua o
+# resultado da operação a 2 variaveis diferentes
+# Soma a uma das variaves o numero 3 e a outra o numero 5
+# ao final mostre o valor das duas variavies
+
+numero = int(input('Digite um numero: '))
+div = numero / 2
+soma1 = div + 3
+soma2 = div + 5
+print(f'O numero lido divido por 2 e somado na primeira variavel a 3 é: {soma1}')
+print(f'O Numero lido divido por 2 e somado na segundo variavel a 5 é: {soma2}')
