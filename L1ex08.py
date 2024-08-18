af17a3a7ad0471712acdeeedb77a5836081bbf9a
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:40:24 -0300

    Add files via upload

---
+++ L1ex08.py
@@ -0,0 +1,13 @@
+# Elaborar um algoritmo que leia tres numeros some 10 ao primeiro
+# subraia 5 do segundo e multiplique o terceiro por 2
+# apos efetuar estas operaçoes mostre o resultado das operaçoes.
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+n3 = int(input('Digite outro numero: '))
+resultado1 = n1 + 10
+resultado2 = n2 - 5
+resultado3 = n3 * 2
+print(f'O resultado do primeiro numero somado a 10 é: {resultado1}')
+print(f'O resultado do segundo numero subtraido por 5 é: {resultado2}')
+print(f'O resultado do terceiro numero multiplicado por 2 é: {resultado3}')
