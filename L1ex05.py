2433432aaab9e2f339189dcbdea183b36c877899
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:39:56 -0300

    Add files via upload

---
+++ L1ex05.py
@@ -0,0 +1,8 @@
+# Crie um algoritmo que leia 2 numeros, subtraia os seus valores (num1 - num2)
+# some ao resultado o numero 10 e mostre o valor da operação em video
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+sub = num1 - num2
+soma = sub + 10
+print(f'O resultado dos numeros subtraidos e somados a 10 é: {soma}')
