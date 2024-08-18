aad27c0a38e20b4c6f525ecb71dba29405ae6b6a
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:39:11 -0300

    Add files via upload

---
+++ L1ex03.py
@@ -0,0 +1,9 @@
+# Crie um algoritmo que leia dois numeros
+# some o numero dois ao valores lidos e mostre os valores resultantes.
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+resultado1 = n1 + 2
+resultado2 = n2 + 2
+print(f'O Resultado do primeiro numero é: {resultado1}')
+print(f'O Resultado do segundo numero é: {resultado2}')
