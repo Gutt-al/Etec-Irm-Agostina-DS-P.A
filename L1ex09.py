98f7266ee5f7d24f9bd52a5ab52754d4652c60a5
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:40:39 -0300

    Add files via upload

---
+++ L1ex09.py
@@ -0,0 +1,7 @@
+# Descrever um algoritmo que leia um numero, subtraia do seu valor o numero 3
+# some o resultado ao numero lido, e mostre o valor final da operação.
+
+numero = int(input('Digite um numero: '))
+sub = numero - 3
+soma = sub + numero
+print(f'O resultado da operação é: {soma}')
