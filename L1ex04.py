aae0f850b5fb03e4b1d09ce81474929bc9de14c5
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:39:20 -0300

    Add files via upload

---
+++ L1ex04.py
@@ -0,0 +1,10 @@
+# Elabore um algoritmo que leia um valor numerico,
+# e some ao seu valor o numero 10, leia um segundo valor, e efetue a sua divisão por dois
+# e mostre os valores resultantes.
+
+n1 = int(input('Digite um numero: '))
+resultado1 = n1 + 10
+n2 = int(input('Digite outro numero: '))
+resultado2 = n2 / 2
+print(f'O resultado do primeiro numero somado a 10 é: {resultado1}')
+print(f'O resultado do segundo numero divido por 2 é? {resultado2}')
