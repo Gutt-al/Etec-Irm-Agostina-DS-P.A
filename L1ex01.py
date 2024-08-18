bc42d283df4f55dd12b90cf818bc52bf65b45e21
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:38:52 -0300

    Add files via upload

---
+++ L1ex01.py
@@ -0,0 +1,6 @@
+# Elaborar um algoritmo que leia um numero
+# some ao valor o numero cinco e exiba o resultado da opeção
+
+numero = int(input('Digite um numero: '))
+resultado = numero + 5
+print(f'O numero digitado + 5 equivale a: {resultado}')
