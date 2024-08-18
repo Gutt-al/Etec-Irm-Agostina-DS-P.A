da74679f0d047efc38fbbabdf798c0bddb35b05b
Gutt-al <contatogutt.al@gmail.com>
2024-08-18 12:39:02 -0300

    Add files via upload

---
+++ L1ex02.py
@@ -0,0 +1,7 @@
+# Descreva um algoritmo que leia um numero, subraia do seu valor o numero tres
+# multiplique por dois e exiba o resultado.
+
+numero = int(input('Digite um numero: '))
+sub = numero - 3
+mult = sub * 2
+print(f'O numero ditado esta sendo subtraido por 3 e multiplicado por 2, seu total é: {mult}')
